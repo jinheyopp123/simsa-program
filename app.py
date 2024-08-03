7efc94064f4fd918b973d85bfc58158ea907aa82
Sindeak or kimjinh1111 <161195769+jinheyopp123@users.noreply.github.com>
2024-08-04 08:20:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,5 @@ def export_to_csv():
     return send_file(file_path, as_attachment=True, download_name='dancer_scores.csv')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
